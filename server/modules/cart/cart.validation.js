26e6c445421b695bebdbd86116c2f2895bb09198
Diwash <diwaskarki96@gmail.com>
2025-02-13 10:16:12 +0545

    cart validation

--- server/modules/cart/cart.validation.js
+++ server/modules/cart/cart.validation.js
@@ -7,3 +7,10 @@ export const addToCartValidation = yup.object({
     .required("Order qunatity is required")
     .min(1, "Order quantity must be atleast 1"),
 });
+
+export const updateCartValidation = yup.object({
+  action: yup
+    .string()
+    .oneOf(["inc", "dec"], "Action must be either inc or dec")
+    .required("Action is required"),
+});
