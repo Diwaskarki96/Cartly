8186785d1f11fcf1a48b51523e1ea460807535ed
Diwash <diwaskarki96@gmail.com>
2025-02-14 17:18:03 +0545

    login and sign in

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,7 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-[1305px] h-[781px] flex  ">
+      <div className="w-[805px] h-[781px]">
+        <Image
+          src="/images/login.png"
+          alt="image"
+          height={681}
+          width={705}
+          priority
+          // className="h-[600px] w-[800px]"
+        />
+      </div>
+      <div className=" m-auto ">
+        <div className="h-[530px] w-[371px] flex flex-col gap-10">
+          <h1 className="font-bold text-xl">Log in to Exclusive</h1>
+          <h1>Enter your details</h1>
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+          <Button className="w-full">Create Account</Button>
+          <h1>
+            Already have account?
+            <Link href="/signin">
+              <span className="underline">Sigin in</span>
+            </Link>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,7 +1,68 @@
+// import Image from "next/image";
+// import React from "react";
+
+// const SigninPage = () => {
+//   return (
+//     <div className="flex gap-8">
+//       <div>
+//         <Image
+//           src="/images/login.png"
+//           alt="image"
+//           priority
+//           width={1200}
+//           height={800}
+//           //style={{ width: "100%", height: "auto" }}
+//         />
+//       </div>
+//       <div className=" flex p-2 w-full">
+//         <div className="flex flex-col items-end">
+//           <h1>Create an Account</h1>
+//           <h1>Enter your details</h1>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SigninPage;
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const SigninPage = () => {
-  return <div>SigninPage</div>;
+  return (
+    <div className="w-[1305px] h-[781px] flex  ">
+      <div className="w-[805px] h-[781px]">
+        <Image
+          src="/images/login.png"
+          alt="image"
+          height={781}
+          width={805}
+          priority
+          // className="h-[600px] w-[800px]"
+        />
+      </div>
+      <div className=" m-auto ">
+        <div className="h-[530px] w-[371px] flex flex-col gap-10">
+          <h1>Create an Account</h1>
+          <h1>Enter your details</h1>
+          <Input type="text" placeholder="Name" />
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
+          <Button className="w-full">Create Account</Button>
+          <h1>
+            Already have account?
+            <Link href="/login">
+              <span className="underline">Log in</span>
+            </Link>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SigninPage;
