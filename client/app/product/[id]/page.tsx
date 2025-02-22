d7b03a94433e6377929a36856471b395c5483335
Diwash <diwaskarki96@gmail.com>
2025-02-22 22:25:31 +0545

    added separator and some style changes

--- client/app/product/[id]/page.tsx
+++ client/app/product/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { $axios } from "@/axios/axiosInstance";
 import Breadcrumbs from "@/components/global/Breadcrumbs";
+import { Separator } from "@/components/ui/separator";
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
@@ -22,13 +23,13 @@ const ProductDetailPage = () => {
       <div>
         <Breadcrumbs />
       </div>
-      <div className="w-full flex">
+      <div className="w-full flex mt-6">
         <div className="w-[60%]">image</div>
         <div>
           <div className="w-full">
             <div className="w-full">
               <p className="font-bold">{productDetails?.name}</p>
-              <div className="flex gap-10 ">
+              <div className="flex gap-14 ">
                 <p className="">${productDetails?.price}</p>
                 {productDetails?.isStock === true ? (
                   <p className="text-xs text-green-600">In Stock</p>
@@ -37,6 +38,7 @@ const ProductDetailPage = () => {
                 )}
               </div>
               <p className="text-sm">{productDetails?.description}</p>
+              <Separator className="mt-4 bg-black dark:bg-red-500" />
             </div>
           </div>
         </div>
