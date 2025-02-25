94ef63ecc97ff7e9e1f65765d14773acf8932fb0
Diwash <diwaskarki96@gmail.com>
2025-02-25 17:51:10 +0545

    font changes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,11 +3,11 @@ import Container from "@/components/global/Container";
 import NavBar from "@/components/navbar/NavBar";
 import { QueryClient } from "@tanstack/react-query";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import ProvidersWrapper from "./ProvidersWrapper";
-const inter = Inter({ subsets: ["latin"] });
+const inter = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Cartly - Your Ultimate Online Shopping Destination",
