40863d9ca33794f45c8a06fb52e417fa4891a82c
Diwash <diwaskarki96@gmail.com>
2025-05-14 11:44:34 +0545

    fix small bug

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const Page = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setIsMounted(true);
+    }
+  }, []);
+
+  if (!isMounted) {
+    return <h1>Loading...</h1>;
+  }
   return <div className="text-2xl">HomePage</div>;
 };
 
-export default page;
+export default Page;
